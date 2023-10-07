9ea3659a8715fde1489aaa08a228667a43ab7244
Huytvph23476 <huytvph23476@fpt.edu.vn>
2023-10-07 23:58:54 +0700

    done giao dien

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route ,Outlet} from "react-router-dom";
+import Header from "./component/header";
+import Footer from "./component/footer";
+import TrangChu1 from "./component/TrangChu1";
+import JordanComponent from "./component/JordanComopnent";
+import Detail from "./component/Detail";
+import GioHang from "./component/GioHang";
+import { Link } from "react-router-dom";
+import Login from "./component/Login";
+import BanHangTaiQuay from "./component/BanHangTaiQuay"
+import BanHangOnlineCoTK from "./component/BanHangOnlineCoTK"
 
+import BanHangOnlineKhongTK from "./component/BanHangOnlineKhongTK"
+import Forgotpassword from "./component/ForgotPassword";
+import SignUp from "./component/SignUp"
+
+
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <BrowserRouter>
+        <Header />  
+        <Routes>
+          <Route path="" exact element={<TrangChu1 />} />
+          <Route path="/login" exact element={<Login/>} />
+          <Route path="/jordan" exact element={<JordanComponent/>} />
+          <Route path='/forgot-password' element={<Forgotpassword/>}/>
+          <Route path="/detail" exact element={<Detail/>} />
+          <Route path="/cart" exact element={<GioHang/>}/>
+          <Route path="/ban-hang-tai-quay" exact element={<BanHangTaiQuay/>}/>
+          <Route path="/ban-hang-online-co-tk" exact element={<BanHangOnlineCoTK/>}/>
+        </Routes>
+        <Footer/>
+       
+      </BrowserRouter>
+   
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Route} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GioHang from './component/GioHang';
+import App2 from "../src/App2.js"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <App2/>
   </React.StrictMode>
 );
 
